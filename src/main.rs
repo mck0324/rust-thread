da4f8b43393640414b6e88e63eb90b4525880cfd
mck0324 <dch3030@naver.com>
2024-10-30 00:15:01 +0900

    Tokio work-stealing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use std::{thread, time::Duration};
+
+use tokio::task;
+
 #[tokio::main(flavor="multi_thread", worker_threads= 4)] //4개의 worker thread 사용
 async fn main() {
-    println!("Hello, world!");
+    println!("Starting tasks...");
+
+    let tasks: Vec<_> = (0..10).map(|i| {
+        task::spawn(async move {
+            println!("Task {} started on {:?}", i, thread::current().id());
+
+            tokio::time::sleep(Duration::from_millis(500)).await;
+            
+            println!("Task {} completed on {:?}", i, thread::current().id());
+        })
+    }).collect();
+
+    for t in tasks {
+        let _ = t.await;
+    }
+
+    println!("All tasks completed.")
 }
